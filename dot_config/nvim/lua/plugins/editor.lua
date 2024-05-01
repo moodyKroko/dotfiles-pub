8123cd2177a21481228b213beccae0697dd21828
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-05-01 18:12:59 +0100

    auto-session disabel use git branch

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -52,15 +52,16 @@ return {
     opts = {
       log_level = "error",
       auto_session_supress_dirs = { "~/", "~/Projects", "~/Downloads", "/", "~" },
+      auto_session_use_git_branch = false,
       pre_save_cmds = { "Neotree close" },
       -- auto_restore_enabled = false,
 
-      session_lens = {
-        buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-        load_on_setup = true,
-        theme_conf = { border = true },
-        previewer = false,
-      },
+      -- session_lens = {
+      --   buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+      --   load_on_setup = true,
+      --   theme_conf = { border = true },
+      --   previewer = false,
+      -- },
     },
     keys = {
       {
