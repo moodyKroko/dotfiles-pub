7d3939ca612359d52d0dc109920f9e83cacf1350
moodykroko <59341541+moodyKroko@users.noreply.github.com>
2024-09-22 20:31:23 +0100

    config fancy notify for lsp progress

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,35 @@
-  -- current color scheme
-  return {
-  {
-    "cpea2506/one_monokai.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
+-- current color scheme
+return {
+	{
+		"cpea2506/one_monokai.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
 
-        -- https://coolors.co/f6c173-703aa6-6d58a7-6ba292-acadbc
-        themes = function(colors)
-          return {
-            border = { fg = colors.purple, bg = colors.none },
-            FloatBorder = { fg = colors.purple, bg = colors.none },
+				-- https://coolors.co/f6c173-703aa6-6d58a7-6ba292-acadbc
+				themes = function(colors)
+					return {
+						border = { fg = colors.purple, bg = colors.none },
+						FloatBorder = { fg = colors.purple, bg = colors.none },
 
-            LineNr = { fg = colors.fg:darken(0.5) },
-            ColorColumn = { bg = colors.purple:darken(0.3) },
-            CursorLine = { bg = colors.none },
-            CursorLineNr = { fg = colors.fg },
-            Search = { fg = colors.black, bg = colors.red },
+						LineNr = { fg = colors.fg:darken(0.5) },
+						ColorColumn = { bg = colors.purple:darken(0.3) },
+						CursorLine = { bg = colors.none },
+						CursorLineNr = { fg = colors.fg },
+						Search = { fg = colors.black, bg = colors.red },
 
-            CmpPmenu = { bg = colors.none },
-            CmpSel = { fg = colors.white, bg = colors.fg:darken(0.5) },
-            CmpDoc = { bg = colors.none },
+						CmpPmenu = { bg = colors.none },
+						CmpSel = { fg = colors.white, bg = colors.fg:darken(0.5) },
+						CmpDoc = { bg = colors.none },
 
-            NoiceCmdlinePopupBorder = { fg = colors.purple, bg = colors.none },
-            DashboardHeader = { fg = colors.aqua },
-          }
-        end,
-      }
-    end,
-  },
+						NoiceCmdlinePopupBorder = { fg = colors.purple, bg = colors.none },
+						DashboardHeader = { fg = colors.aqua },
+						NotifyBackground = { bg = colors.white },
+					}
+				end,
+			}
+		end,
+	},
 }
