49862d2501a2e420ee9e9c965232240efa49dd0f
moodykroko <moodykroko@gmail.com>
2024-09-22 16:40:56 +0100

    feat: keep visual popup to its original place

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,122 +1,150 @@
 return {
-  -- disabled UI packages
-  { "indent-blankline.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  {
-    "echasnovski/mini.indentscope",
-    enabled = false,
-  },
+	-- disabled UI packages
+	{ "indent-blankline.nvim", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
+	{
+		"echasnovski/mini.indentscope",
+		enabled = false,
+	},
 
-  {
-    "folke/noice.nvim",
-    lazy = true,
-    opts = {
-      cmdline = {
-        view = "cmdline",
-      },
-    },
-  },
+	{
+		"folke/noice.nvim",
+		lazy = true,
+		opts = {
+			cmdline = {
+				view = "cmdline_popup",
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = "100%",
+					},
+					size = {
+						height = "auto",
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+					win_options = {
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+					},
+				},
+			},
+		},
+	},
 
-  -- status line
-  {
-    "nvim-lualine/lualine.nvim",
-    -- enabled = false,
-    opts = {
-      options = {
-        theme = "papercolor_light",
-      },
-      sections = {
-        lualine_c = {
-          {
-            "diagnostics",
-            symbols = {
-              error = " ",
-              warn = "󰀪 ",
-              info = "󰌶 ",
-              hint = " ",
-            },
-          },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { LazyVim.lualine.pretty_path() },
-        },
-      },
-    },
-  },
+	-- status line
+	{
+		"nvim-lualine/lualine.nvim",
+		-- enabled = false,
+		opts = {
+			options = {
+				theme = "papercolor_light",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"diagnostics",
+						symbols = {
+							error = " ",
+							warn = "󰀪 ",
+							info = "󰌶 ",
+							hint = " ",
+						},
+					},
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ LazyVim.lualine.pretty_path() },
+				},
+			},
+		},
+	},
 
-  -- Notifications system
-  {
-    "rcarriga/nvim-notify",
-    lazy = true,
-    opts = {
-      timeout = 3000,
-      stages = "static",
-      render = "default", -- "compact"
-      background_colour = "#56B6C2", --#a9ff68
-    },
-  },
+	-- Notifications system
+	{
+		"rcarriga/nvim-notify",
+		lazy = true,
+		opts = {
+			timeout = 3000,
+			stages = "static",
+			render = "default", -- "compact"
+			background_colour = "#56B6C2", --#a9ff68
+		},
+	},
 
-  -- rainbow brackets
-  {
-    "hiphish/rainbow-delimiters.nvim",
-    event = "BufReadPre",
-  },
+	-- rainbow brackets
+	{
+		"hiphish/rainbow-delimiters.nvim",
+		event = "BufReadPre",
+	},
 
-  -- mason signs
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-        border = "rounded",
-      },
-    },
-  },
+	-- mason signs
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+				border = "rounded",
+			},
+		},
+	},
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      icons = {
-        diagnostics = {
-          Error = " ",
-        },
-      },
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			icons = {
+				diagnostics = {
+					Error = " ",
+				},
+			},
+		},
+	},
 
-  {
-    "nvim-lspconfig",
-    opts = {
-      diagnostics = {
-        virtual_text = true,
-        update_in_insert = false,
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
-        },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = " ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-            [vim.diagnostic.severity.INFO] = " ",
-          },
-        },
-      },
-    },
-  },
+	{
+		"nvim-lspconfig",
+		opts = {
+			diagnostics = {
+				virtual_text = true,
+				update_in_insert = false,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = "󰀪 ",
+						[vim.diagnostic.severity.HINT] = "󰌶 ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+				},
+			},
+		},
+	},
 
-  {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      local logo = [[
+	{
+		"nvimdev/dashboard-nvim",
+		opts = function(_, opts)
+			local logo = [[
 ███╗   ███╗ ██████╗  ██████╗ ██████╗ ██╗   ██╗██╗  ██╗██████╗  ██████╗ ██╗  ██╗ ██████╗ 
 ████╗ ████║██╔═══██╗██╔═══██╗██╔══██╗╚██╗ ██╔╝██║ ██╔╝██╔══██╗██╔═══██╗██║ ██╔╝██╔═══██╗
 ██╔████╔██║██║   ██║██║   ██║██║  ██║ ╚████╔╝ █████╔╝ ██████╔╝██║   ██║█████╔╝ ██║   ██║
@@ -125,8 +153,8 @@ return {
 ╚═╝     ╚═╝ ╚═════╝  ╚═════╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ 
                                                                                         
       ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-    end,
-  },
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
+			opts.config.header = vim.split(logo, "\n")
+		end,
+	},
 }
