93c0e60a82dab9d30d88b4dc05719845b14567e7
moodykroko <moodykroko@gmail.com>
2024-09-22 16:18:06 +0100

    update: add description to conform format

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,54 +1,55 @@
 return {
-  -- tools
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "shellcheck",
-        -- "luacheck",
-        -- "tailwindcss-language-server",
-        -- "typescript-language-server",
-        "gopls",
-        "goimports",
-        "gofumpt",
-        "delve",
-        "gomodifytags",
-        "impl",
-        "json-lsp",
-        "css-lsp",
-      })
-    end,
-  },
+	-- tools
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"shellcheck",
+				-- "luacheck",
+				-- "tailwindcss-language-server",
+				-- "typescript-language-server",
+				"gopls",
+				"goimports",
+				"gofumpt",
+				"delve",
+				"gomodifytags",
+				"impl",
+				"json-lsp",
+				"css-lsp",
+			})
+		end,
+	},
 
-  -- lsp servers
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+	-- lsp servers
+	{
+		"neovim/nvim-lspconfig",
+		opts = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
-      keys[#keys + 1] = {
-        "gr",
-        "<cmd>Trouble lsp_references toggle focus=true auto_jump=true<cr>",
-        mode = { "n" },
-        desc = "LSP References (Trouble)",
-        has = "reference"
-      }
+			keys[#keys + 1] = {
+				"gr",
+				"<cmd>Trouble lsp_references toggle focus=true auto_jump=true<cr>",
+				mode = { "n" },
+				desc = "LSP References (Trouble)",
+				has = "reference",
+			}
 
-      keys[#keys + 1] = {
-        "gd",
-        "<cmd>Trouble lsp_definitions toggle focus=true auto_jump=true<cr>",
-        mode = { "n" },
-        desc = "Goto Definition (Trouble)",
-        has = "definition"
-      }
+			keys[#keys + 1] = {
+				"gd",
+				"<cmd>Trouble lsp_definitions toggle focus=true auto_jump=true<cr>",
+				mode = { "n" },
+				desc = "Goto Definition (Trouble)",
+				has = "definition",
+			}
 
-      keys[#keys+1] = {
-        "<leader>cf",
-        mode = { "n", "v"},
-        function()
-          require("conform").format {async = true, lsp_fallback = true }
-        end,
-      }
-    end,
-  },
+			keys[#keys + 1] = {
+				"<leader>cf",
+				mode = { "n", "v" },
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				desc = "Conform format",
+			}
+		end,
+	},
 }
