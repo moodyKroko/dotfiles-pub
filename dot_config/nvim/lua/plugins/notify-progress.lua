b39269d62c23a421704d9c710425565e215c2b68
moodykroko <59341541+moodyKroko@users.noreply.github.com>
2024-09-22 21:23:50 +0100

    fix properly return a table from nvim-notify config and formatting

--- dot_config/nvim/lua/plugins/notify-progress.lua
+++ dot_config/nvim/lua/plugins/notify-progress.lua
@@ -1,7 +1,9 @@
 -- Utility functions shared between progress reports for LSP and DAP
+local M = {}
+
 local client_notifs = {}
 
-local function get_notif_data(client_id, token)
+function M.get_notif_data(client_id, token)
 	if not client_notifs[client_id] then
 		client_notifs[client_id] = {}
 	end
@@ -15,8 +17,8 @@ end
 
 local spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
 
-local function update_spinner(client_id, token)
-	local notif_data = get_notif_data(client_id, token)
+function M.update_spinner(client_id, token)
+	local notif_data = M.get_notif_data(client_id, token)
 
 	if notif_data.spinner then
 		local new_spinner = (notif_data.spinner + 1) % #spinner_frames
@@ -29,23 +31,22 @@ local function update_spinner(client_id, token)
 		})
 
 		vim.defer_fn(function()
-			update_spinner(client_id, token)
+			M.update_spinner(client_id, token)
 		end, 100)
 	end
 end
 
-local function format_title(title, client_name)
+function M.format_title(title, client_name)
 	return client_name .. (#title > 0 and ": " .. title or "")
 end
 
-local function format_message(message, percentage)
+function M.format_message(message, percentage)
 	return (percentage and percentage .. "%\t" or "") .. (message or "")
 end
 
 -- LSP integration
 -- Make sure to also have the snippet with the common helper functions in your config!
-
-function Progress()
+function M.progress()
 	vim.lsp.handlers["$/progress"] = function(_, result, ctx)
 		local client_id = ctx.client_id
 
@@ -55,27 +56,27 @@ function Progress()
 			return
 		end
 
-		local notif_data = get_notif_data(client_id, result.token)
+		local notif_data = M.get_notif_data(client_id, result.token)
 
 		if val.kind == "begin" then
-			local message = format_message(val.message, val.percentage)
+			local message = M.format_message(val.message, val.percentage)
 
 			notif_data.notification = vim.notify(message, "info", {
-				title = format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
+				title = M.format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
 				icon = spinner_frames[1],
 				timeout = false,
 				hide_from_history = false,
 			})
 
 			notif_data.spinner = 1
-			update_spinner(client_id, result.token)
+			M.update_spinner(client_id, result.token)
 		elseif val.kind == "report" and notif_data then
-			notif_data.notification = vim.notify(format_message(val.message, val.percentage), "info", {
+			notif_data.notification = vim.notify(M.format_message(val.message, val.percentage), "info", {
 				replace = notif_data.notification,
 				hide_from_history = false,
 			})
 		elseif val.kind == "end" and notif_data then
-			notif_data.notification = vim.notify(val.message and format_message(val.message) or "Complete", "info", {
+			notif_data.notification = vim.notify(val.message and M.format_message(val.message) or "Complete", "info", {
 				icon = "",
 				replace = notif_data.notification,
 				timeout = 3000,
@@ -86,6 +87,4 @@ function Progress()
 	end
 end
 
-return {
-  Progress()
-}
+return M
