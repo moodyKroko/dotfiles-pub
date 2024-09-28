42f619a48c2d8ff058bc5ec9de24e008226c5d4c
moodykroko <59341541+moodyKroko@users.noreply.github.com>
2024-09-28 19:53:35 +0100

    update colorscheme to use tokyonight

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,58 +1,73 @@
 -- current color scheme
 return {
-	{
-		"cpea2506/one_monokai.nvim",
-		lazy = false,
-    enabled = false,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-
-				-- https://coolors.co/f6c173-703aa6-6d58a7-6ba292-acadbc
-				themes = function(colors)
-					return {
-						border = { fg = colors.purple, bg = colors.none },
-						FloatBorder = { fg = colors.purple, bg = colors.none },
-
-						LineNr = { fg = colors.fg:darken(0.5) },
-						ColorColumn = { bg = colors.purple:darken(0.3) },
-						CursorLine = { bg = colors.none },
-						CursorLineNr = { fg = colors.fg },
-						Search = { fg = colors.black, bg = colors.red },
-
-						CmpPmenu = { bg = colors.none },
-						CmpSel = { fg = colors.white, bg = colors.fg:darken(0.5) },
-						CmpDoc = { bg = colors.none },
-
-						NoiceCmdlinePopupBorder = { fg = colors.purple, bg = colors.none },
-						DashboardHeader = { fg = colors.aqua },
-						NotifyBackground = { bg = colors.white },
-					}
-				end,
-			}
-		end,
-	},
-
-	{
-    "rebelot/kanagawa.nvim",
+  {
+    "cpea2506/one_monokai.nvim",
     lazy = false,
+    enabled = false,
     priority = 1000,
-    config = function ()
+    opts = function()
       return {
-        require("kanagawa").setup({
-          transparent = true,
-          functionStyle = { bold = true },
-        })
+        transparent = true,
+
+        -- https://coolors.co/f6c173-703aa6-6d58a7-6ba292-acadbc
+        themes = function(colors)
+          return {
+            border = { fg = colors.purple, bg = colors.none },
+            FloatBorder = { fg = colors.purple, bg = colors.none },
+
+            LineNr = { fg = colors.fg:darken(0.5) },
+            ColorColumn = { bg = colors.purple:darken(0.3) },
+            CursorLine = { bg = colors.none },
+            CursorLineNr = { fg = colors.fg },
+            Search = { fg = colors.black, bg = colors.red },
+
+            CmpPmenu = { bg = colors.none },
+            CmpSel = { fg = colors.white, bg = colors.fg:darken(0.5) },
+            CmpDoc = { bg = colors.none },
+
+            NoiceCmdlinePopupBorder = { fg = colors.purple, bg = colors.none },
+            DashboardHeader = { fg = colors.aqua },
+            NotifyBackground = { bg = colors.white },
+          }
+        end,
       }
-    end
+    end,
   },
 
-	{
-		"LazyVim/LazyVim",
-		opts = {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      return {
+        require("tokyonight").setup({
+          style = "night",
+          transparent = true,
+          styles = {
+            functions = { bold = true },
+            sidebars = "transparent",
+            floats = "transparent",
+          },
+
+          ---@param highlights tokyonight.Highlights
+          ---@param colors ColorScheme
+          on_highlights = function (highlights, colors)
+            highlights.CmpPmenu = { bg = colors.none }
+            highlights.CmpSel = { fg = colors.fg, bg = '#292e42' }
+            highlights.CmpDoc = { bg = colors.none }
+          end
+
+        }),
+      }
+ end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
       -- colorscheme = "one_monokai",
-      colorscheme = "kanagawa",
-		},
-	},
+      colorscheme = "tokyonight",
+    },
+  },
 }
