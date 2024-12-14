5a85a24bfacdf8d61e4567b79edb18a238ecb79a
moodykroko <59341541+moodyKroko@users.noreply.github.com>
2024-12-14 19:38:34 +0000

    update revert changes

--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -1,32 +1,24 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = function(_, opts)
-			opts.formatters_by_ft = {
-				astro = { "prettier" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				vue = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				less = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				jsonc = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				handlebars = { "prettier" },
-				-- go = { "goimports", "gofmt" },
-			}
-
-			opts.formatters = opts.formatters or {}
-			opts.formatters.prettier = {
-				filetypes = { "astro" },
-				prepend_args = { "--plugin", "prettier-plugin-astro", "--plugin", "prettier-plugin-tailwindcss" },
-			}
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      astro = { "prettier" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      vue = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      less = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      graphql = { "prettier" },
+      handlebars = { "prettier" },
+      -- go = { "goimports", "gofmt" },
+    }
+  },
 }
