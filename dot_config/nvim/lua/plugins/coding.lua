35ac9a72b3a4743ad9f2a5e49a92503fbe7a519d
moodykroko <moodykroko@gmail.com>
2025-01-04 15:12:37 +0000

    update to blink.cmp

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -11,57 +11,78 @@ return {
     end,
   },
 
-  -- then: setup supertab in cmp
   {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded",
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+        },
+      },
 
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      opts.preselect = cmp.PreselectMode.Item
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<CR>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            if luasnip.expandable() then
-              luasnip.expand()
-            else
-              cmp.confirm({
-                select = true,
-              })
-            end
-          else
-            fallback()
-          end
-        end),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-
-      -- opts.experimental = {
-      --   ghost_text = false,
-      -- }
-    end,
+      signature = {
+        window = {
+          border = "rounded",
+        },
+      },
+    },
   },
+
+  -- then: setup supertab in cmp
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     local cmp = require("cmp")
+  --     local luasnip = require("luasnip")
+  --
+  --     opts.preselect = cmp.PreselectMode.Item
+  --     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --       ["<CR>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           if luasnip.expandable() then
+  --             luasnip.expand()
+  --           else
+  --             cmp.confirm({
+  --               select = true,
+  --             })
+  --           end
+  --         else
+  --           fallback()
+  --         end
+  --       end),
+  --       ["<Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.select_next_item()
+  --         elseif vim.snippet.active({ direction = 1 }) then
+  --           vim.schedule(function()
+  --             vim.snippet.jump(1)
+  --           end)
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --       ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.select_prev_item()
+  --         elseif vim.snippet.active({ direction = -1 }) then
+  --           vim.schedule(function()
+  --             vim.snippet.jump(-1)
+  --           end)
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --     })
+  --
+  --     -- opts.experimental = {
+  --     --   ghost_text = false,
+  --     -- }
+  --   end,
+  -- },
 }
