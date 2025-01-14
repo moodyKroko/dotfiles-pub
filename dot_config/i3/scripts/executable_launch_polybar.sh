32c4271b73ee313133ed9ee4202ccf297ec27415
moodykroko <59341541+moodyKroko@users.noreply.github.com>
2025-01-14 23:29:46 +0000

    update fix

--- dot_config/i3/scripts/executable_launch_polybar.sh
+++ dot_config/i3/scripts/executable_launch_polybar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # launch bar and bar2
-polybar --config="$HOME/.config/i3/scripts/launch_polybar.sh" --reload moody &
+polybar --config="~/.config/i3/scripts/launch_polybar.sh" --reload moody &
