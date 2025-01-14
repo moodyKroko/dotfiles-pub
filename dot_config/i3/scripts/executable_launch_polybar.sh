58763edcd6508bbf187e7951e5a1b949205ca1f0
moodykroko <59341541+moodyKroko@users.noreply.github.com>
2025-01-14 23:28:13 +0000

    update fix

--- dot_config/i3/scripts/executable_launch_polybar.sh
+++ dot_config/i3/scripts/executable_launch_polybar.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# wait until processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do
-sleep 1; done
-
 # launch bar and bar2
-polybar --config="$HOME/.config/i3/scripts/launch_polybar.sh" moody &
+polybar --config="$HOME/.config/i3/scripts/launch_polybar.sh" --reload moody &
