f2cba6d30bc6316502b6745b1895a9fbf4e9b2bf
moodyKroko <59341541+moodyKroko@users.noreply.github.com>
2025-02-09 18:46:49 +0000

    update noice to not show lsp progress

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -4,47 +4,114 @@ return {
 
   {
     "folke/noice.nvim",
-    opts = {
-      cmdline = {
-        -- enabled = true, -- disable to use default cmdline
-        view = "cmdline_input2",
-      },
+    opts = function(_, opts)
+      opts.debug = false
+      opts.routes = opts.routes or {}
 
-      lsp = {
+      opts.presets = { lsp_doc_border = true }
+
+      opts.lsp = {
         hover = {
           silent = true, -- set to true to not show a message if hover is not available
         },
-      },
+      }
 
-      views = {
-        cmdline_input2 = {
-          view = "cmdline_popup",
-          position = { row = "97%", col = "5%" },
-          border = { style = "rounded", padding = { 0, 1 } },
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
         },
+        opts = { skip = true },
+      })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
 
-        popupmenu = {
-          relative = "editor",
-          position = {
-            row = "86%",
-            col = "2%",
+      table.insert(opts.routes, 1, {
+        filter = {
+          ["not"] = {
+            event = "lsp",
+            kind = "progress",
           },
-          border = {
-            style = "rounded",
-          },
-          win_options = {
-            winhighlight = "Normal:Normal,TelescopeBorder:Normal",
-          },
+          cond = function()
+            return not focused and false
+          end,
         },
-      },
+        view = "notify_send",
+        opts = { stop = false, replace = true },
+      })
 
-      presets = {
-        bottom_search = false,
-        command_palette = false,
-        lsp_doc_border = true,
-      },
-    },
+      -- ignore lsp servers progress message
+      table.insert(opts.routes, {
+        filter = {
+          event = "lsp",
+          kind = "progress",
+        },
+        opts = { skip = true },
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+      return opts
+    end,
   },
+  -- {
+  -- "folke/noice.nvim",
+  -- opts = {
+  --   cmdline = {
+  --     -- enabled = true, -- disable to use default cmdline
+  --     view = "cmdline_input2",
+  --   },
+  --
+  --   lsp = {
+  --     hover = {
+  --       silent = true, -- set to true to not show a message if hover is not available
+  --     },
+  --   },
+  --
+  --   views = {
+  --     cmdline_input2 = {
+  --       view = "cmdline_popup",
+  --       position = { row = "97%", col = "5%" },
+  --       border = { style = "rounded", padding = { 0, 1 } },
+  --     },
+  --
+  --     popupmenu = {
+  --       relative = "editor",
+  --       position = {
+  --         row = "86%",
+  --         col = "2%",
+  --       },
+  --       border = {
+  --         style = "rounded",
+  --       },
+  --       win_options = {
+  --         winhighlight = "Normal:Normal,TelescopeBorder:Normal",
+  --       },
+  --     },
+  --   },
+  --
+  --   presets = {
+  --     bottom_search = false,
+  --     command_palette = false,
+  --     lsp_doc_border = true,
+  --   },
+  -- },
+  -- },
 
   -- status line
   {
